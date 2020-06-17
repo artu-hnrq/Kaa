37c523a36c06002646e93f0efe03ac27ab9198f8
Arthur Henrique Della Fraga <Arthur.Henrique.Della.Fraga@gmail.com>
2020-06-17 20:39:52 -0300

    ci(code-style) Kaa is maturing the way it behaves during his commitments

---
+++ src/kaa/utils/cmd.py
@@ -0,0 +1,24 @@
+import subprocess
+from typing import NamedTuple
+
+# Code from Commitizen open-source project
+# https://github.com/commitizen-tools/commitizen/blob/master/commitizen/cmd.py
+
+
+class Command(NamedTuple):
+    out: str
+    err: str
+    stdout: bytes
+    stderr: bytes
+
+
+def run(cmd: str) -> Command:
+    process = subprocess.Popen(
+        cmd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+    )
+    stdout, stderr = process.communicate()
+    return Command(stdout.decode(), stderr.decode(), stdout, stderr)
--- src/kaa/viper/venom.py
+++ src/kaa/viper/venom.py
@@ -2,39 +2,39 @@ from .. import utils
 from distutils import cmd
 import os
 
+
 class Attack(cmd.Command):
-    description = 'Run all functions declared in the kaa.attack entry point group'
+    description = "Run all functions declared in the kaa.attack entry point group"
     user_options = []
 
     def initialize_options(self):
-        self.fang = utils.package_discovery('kaa.attack')
+        self.fang = utils.package_discovery("kaa.attack")
 
     def finalize_options(self):
         pass
 
     def run(self):
-        for key, func  in self.fang.items():
-            print('kaa attack:', key)
+        for key, func in self.fang.items():
+            print("kaa attack:", key)
             func()
 
 
 class Shed(cmd.Command):
-    description = 'Bump up package version'
+    description = "Bump up package version"
     user_options = [
-        ('bump=', 'b', 'version bump group'),
+        ("bump=", "b", "version bump group"),
     ]
 
     def initialize_options(self):
-        self.bump = 'build'
+        self.bump = "BUILD"
 
     def finalize_options(self):
         assert self.bump in [
-            'major', 'minor', 'patch', 'build'
+            "MAJOR",
+            "MINOR",
+            "PATCH",
+            "BUILD",
         ]
 
     def run(self):
-        os.system(' '.join([
-            "bumpversion",
-            "--allow-dirty",
-            self.bump,
-        ]))
+        os.system(" ".join(["bumpversion", "--allow-dirty", self.bump,]))
